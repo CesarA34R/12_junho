62f4edaacc3d65848bffa7e022bd2e8a61a7fcd4
CesarA34R <rodricesaraugustocarvalho@gmail.com>
2024-05-06 16:21:12 -0300

    up

--- js/validar_formulario.js
+++ js/validar_formulario.js
@@ -1,16 +1,3 @@
-<<<<<<< Updated upstream
-if (window.location.href.indexOf("POST") !== -1) {
-     var opcao1 = document.querySelector('input[name="opcao1"]').value;
-     var opcao2 = document.querySelector('input[name="opcao2"]').value;
-     var opcao3 = document.querySelector('input[name="opcao3"]').value;
- 
-     if (opcao1 === 'b_break' && opcao2 === 'meu_universo' && opcao3 === 'lanche_break') {
-         window.location.href = '../view/sucesso.html';
-     } else {
-         window.location.href = '../view/tente_novamente.html';
-     }
- }
-=======
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById('quizForm');
     const submitButton = document.getElementById('submitButton');
